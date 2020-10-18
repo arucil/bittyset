0ee8e85f040608c2701235eecc043270a709fc90
amlo <xplzjwz@gmail.com>
2020-10-18 10:31:30 +0800

    feat: support specifying generic parameter of BitSet for bitset macro

--- src/macros.rs
+++ src/macros.rs
@@ -10,11 +10,26 @@
 /// assert_eq!(set.len(), 4);
 /// assert_eq!(format!("{:?}", set), "{2, 4, 7, 10}");
 ///
-/// let set2 = bitset![2,7,10,4,7];
+/// // The generic parameter of BitSet can be specified explicitly.
+/// let set2 = bitset![usize; 2,7,10,4,7];
 /// assert_eq!(set, set2);
 /// ```
 #[macro_export]
 macro_rules! bitset {
+  ($t:ty $(;)?) => {
+    let mut set = <$crate::BitSet::<$t>>::new();
+    $(
+      set.insert($elem);
+    )*
+    set
+  };
+  ($t:ty ; $($elem:expr),* $(,)?) => {{
+    let mut set = <$crate::BitSet>::new();
+    $(
+      set.insert($elem);
+    )*
+    set
+  }};
   () => {
     <$crate::BitSet>::new()
   };
@@ -24,5 +39,5 @@ macro_rules! bitset {
       set.insert($elem);
     )*
     set
-  }}
+  }};
 }
